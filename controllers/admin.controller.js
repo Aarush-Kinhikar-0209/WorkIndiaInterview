36105d2276bbe228823bcc7f014f1f04c40fc622
aarush <aarush.kinhikar@gmail.com>
2024-07-02 10:58:28 +0530

    admin login

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -2,7 +2,7 @@ const pool = require('../config/database')
 
 const login = async (req, res) => {
     try {
-        const { car_id, origin, destination, hours, requirement } = req.body
+
     } catch (error) {
         return res.json({
             "success": false,
